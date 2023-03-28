10b3fbfbcf6f34b3396e42e23e1ede38f6bff192
tartagliajk <92087041+tartagliajk@users.noreply.github.com>
2023-03-29 01:40:28 +0200

    Add files via upload

---
+++ library-main/biblotek/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace AdminHandlesBook
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Availability { get; set; }
+
+        public Book(string bookId, string bookAuthor, string bookName, int bookYear, int bookavailabilty) {
+            Id = bookId;
+            Author = bookAuthor;
+            Name = bookName;
+            Year = bookYear;
+            Availability = bookavailabilty;
+        }
+    }
+}
