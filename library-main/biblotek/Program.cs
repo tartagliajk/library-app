51091e596ff47de4f90d1cc7aeb7f3015e54aa2a
tartagliajk <92087041+tartagliajk@users.noreply.github.com>
2023-03-29 08:29:21 +0200

    Add files via upload

---
+++ library-main/biblotek/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using AdminHandlesBook;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace biblotek
+{
+    internal class Program
+    {
+        static BookSystem system = BookSystem.GetInstance();
+        //Gives a key to member so admin and member dont have the same menu
+        static int memberKey;
+        static void Main(string[] args)
+        {
+            selection();
+        }
+
+        static void selection()
+        {
+            Console.WriteLine("Welcome to the library");
+            Console.WriteLine("Please enter your option");
+            Console.WriteLine("Log - Login; Sign - Signup; C - Close");
+            var Option = Console.ReadLine();
+            //makes it lowercase
+            Option = Option.ToLower();
+            if (Option == "log")
+            {
+                login.LoginPage();
+            }
+            else if (Option == "sign")
+            {
+                signup.signupPage();
+            }else if (Option == "c")
+            {
+                //closes the application
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Enter a valid option");
+                selection();
+            }
+        }
+
+        static public void memberMenu()
+        {
+            Console.WriteLine("You are now loged in as a member, please pick ur next option");
+            Console.WriteLine("List - List all books; Search - Search; C - close");
+            memberKey = 1;
+            var Option = Console.ReadLine();
+            Option = Option.ToLower();
+            if (Option == "list")
+            {
+                ListAllBooks();
+            }
+            else if (Option == "search")
+            {
+                Search();
+            }
+            else if (Option == "c")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Enter a valid option");
+                memberMenu();
+            }
+        }
+
+        static public void adminMenu()
+        {
+            Console.WriteLine("\nYou are now loged in as a admin, please pick ur next option");
+            Console.WriteLine("Listb - List all books; Listm - List all members; Search - Search; Add - Add books; Change - Change Password; C - close");
+            var Option = Console.ReadLine();
+            Option = Option.ToLower();
+            if (Option == "listb")
+            {
+                ListAllBooks();
+            }else if (Option == "listm")
+            {
+                ListAllMembers();
+            }
+            else if (Option == "add")
+            {
+                AddBook();
+            }
+            else if (Option == "search")
+            {
+                Search();
+            }
+            else if(Option == "change")
+            {
+                changePassword();
+            }
+            else if (Option == "c")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Enter a valid option");
+                adminMenu();
+            }
+        }
+        static void AddBook()
+        {
+            Console.Clear();
+            Console.WriteLine("Add a new book");
+
+            Console.WriteLine("Enter book information. Put a '-' between spaces.\n");
+
+            var id = (system.GetBooks().Count + 1).ToString();
+
+            Console.Write("Author: ");
+            var author = Console.ReadLine();
+
+            Console.Write("Name: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Year: ");
+            var year = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Availability (1 - true; 0 - false): ");
+            var availability = Int32.Parse(Console.ReadLine());
+
+            Book newBook = new Book(id, author, name, year, availability);
+            system.AddBook(newBook);
+
+            adminMenu();
+        }
+
+        static void ListAllBooks()
+        {
+            Console.Clear();
+            List<Book> books = system.GetBooks();
+
+            //sets index as i which represents the lines
+            //checks lines in file and add 1+ everytime it prints
+            for (var i = 0; i < books.Count; i++)
+            {
+                Book book = books[i];
+
+                Console.WriteLine($"{book.Id} {book.Author} {book.Name} {book.Year} {book.Availability}");
+            }
+
+            if(memberKey == 1)
+            {
+                memberMenu();
+            }
+            else
+            {
+                adminMenu();
+            }
+            
+        }
+
+        static void ListAllMembers()
+        {
+            Console.Clear();
+            string text = System.IO.File.ReadAllText(@"C:\Users\jeonn\OneDrive\Desktop\library-main\biblotek\details.txt");
+            System.Console.WriteLine("All members = {0}\n", text);
+
+            adminMenu();
+        }
+
+        static void Search()
+        {
+            Console.Write("Search after a book: ");
+            var query = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            //two ways to console clear
+            Console.WriteLine("\u001b[2J\u001b[3J");
+            Console.Clear();
+
+            Console.WriteLine($"Search after a book: {query}\n");
+
+            var result = system.FindBook(query);
+
+            for (var i = 0; i < result.Count; i++)
+            {
+                var book = result[i];
+
+                Console.WriteLine($"Author: {book.Author}");
+                Console.WriteLine($"Name: {book.Name}");
+                Console.WriteLine($"Year:{book.Year}");
+            }
+
+            if (memberKey == 1)
+            {
+                memberMenu();
+            }
+            else
+            {
+                adminMenu();
+            }
+        }
+
+        static void changePassword()
+        {
+            Console.WriteLine("Write your SSN");
+            var writtenSSN = Console.ReadLine();
+            Console.WriteLine("\nWrite your new password");
+            var writtenPassword = Console.ReadLine();
+
+            //reads the file and spilts ","
+            var users = File.ReadAllText(@"C:\Users\jeonn\OneDrive\Desktop\library-main\biblotek\details.txt").Split(',').ToList().Where(x => !String.IsNullOrWhiteSpace(x));
+
+            List<User> myUsers = new List<User>();
+            foreach (var user in users)
+            {
+                var information = user.Split(' ');
+                User temp = new User();
+                temp.Name = information[0].Trim();
+                temp.lastName = information[1].Trim();
+                temp.SSN = information[2].Trim();
+                temp.Password = information[3].Trim();
+                temp.status = information[4].Trim();
+                myUsers.Add(temp);
+            }
+
+            //checks if it match and then change the password
+            var selectedUser = myUsers.Where(x => x.SSN == writtenSSN).SingleOrDefault();
+            myUsers.Remove(selectedUser);
+            selectedUser.Password = writtenPassword;
+            myUsers.Add(selectedUser);
+
+            List<string> formatForFile = new List<string>();
+            foreach (var item in myUsers)
+            {
+                formatForFile.Add(item.PrepareForFile());
+            }
+            File.WriteAllLines(@"C:\Users\jeonn\OneDrive\Desktop\library-main\biblotek\details.txt", formatForFile.ToArray());
+
+            Console.WriteLine("The password has been changed");
+        }
+            
+
+        private class User
+        {
+            public string Name { get; set; }
+            public string lastName { get; set; }
+            public string SSN { get; set; }
+            public string Password { get; set; }
+            public string status { get; set; }
+
+            public string PrepareForFile()
+            {
+                return Name + " " + lastName + " " + SSN + " " + Password + " " + status + " ,";
+            }
+        }
+        
+    }
+}
---
+++ library-main/biblotek/signup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblotek
+{
+    internal class signup
+    {
+        static public void signupPage()
+        {
+            Console.WriteLine("\nTo signup insert First name, Last name, Ssn and password.");
+            Console.WriteLine("");
+
+            Console.Write("First Name: ");
+            var firstName = Console.ReadLine();
+
+            Console.Write("Last Name: ");
+            var lastName = Console.ReadLine();
+
+            Console.Write("Ssn: ");
+            var personalNumber = Console.ReadLine();
+
+            Console.Write("Password: ");
+            var password = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            //if any of the values are null:
+            if (UserInfoIncomplete(firstName, lastName, personalNumber, password))
+            {
+                Console.Clear();
+                Console.WriteLine("Insert real info to signup.");
+                Console.WriteLine("");
+                signupPage();
+            }//if the ssn already exist:
+            else if (UserRegistered(personalNumber))
+            {
+                Console.Clear();
+                Console.WriteLine("You are already registerd, please insert correct infomartion.");
+                Console.WriteLine("");
+                signupPage();
+            }
+
+            var line = $"{firstName} {lastName} {personalNumber} {password} {"medlem"} {","}";
+            string[] lines = { line };
+
+            File.AppendAllLines(@"C:\Users\jeonn\OneDrive\Desktop\library-main\biblotek\details.txt", lines);
+
+            Console.WriteLine("You are now signed up and can login. Please wait while we redirect you.");
+
+            Thread.Sleep(3000);
+
+            login.LoginPage();
+        }
+
+
+        static bool UserRegistered(string personalNumber)
+        {
+            string[] users = System.IO.File.ReadAllLines(@"C:\Users\jeonn\OneDrive\Desktop\library-main\biblotek\details.txt");
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                var line = users[i].Trim();
+                string[] parts = line.Split(' ');
+
+                var currentPersonalNumber = parts[2];
+
+                if (currentPersonalNumber == personalNumber)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static bool UserInfoIncomplete(string? firstName, string? lastName, string? personalNumber, string? password)
+        {
+            if (firstName == null || firstName == "") return true;
+            if (lastName == null || lastName == "") return true;
+            if (personalNumber == null || personalNumber == "") return true;
+            if (password == null || password == "") return true;
+
+            return false;
+        }
+    }
+}
